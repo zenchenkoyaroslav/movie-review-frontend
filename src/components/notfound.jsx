20a45f82fa3296aaac8796f02fe0c92bb8deb0d9
zenchenkoyaroslav <47921331+zenchenkoyaroslav@users.noreply.github.com>
2020-05-22 01:08:06 +0200

    added 404 component

---
+++ src/components/notfound.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Notfound extends Component {
+
+  render() {
+    return(
+      <div className="col-md-8 col-lg-9">
+        <div className="row">
+          <h2 className="w-100 text-center" style={{fontWeight: 100, fontSize: 48, color: "rgba(1,1,1,.4)"}}>404 not found</h2>
+        </div>
+      </div>);
+  }
+}
+
